27e3b2088b66da5ae613b9bd17177af9233e6ed5
PritamPrajarpathi <pritamprajarpathi@gmail.com>
2023-07-28 16:49:54 +0630

    exe and draw face

---
+++ drawface.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+
+# For webcam input:
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+cap = cv2.VideoCapture(0)
+
+max_num_faces = 3  # Change this value to the maximum number of faces you want to detect
+
+with mp_face_mesh.FaceMesh(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    max_num_faces=max_num_faces) as face_mesh:
+    
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = face_mesh.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACE_CONNECTIONS,
+                    landmark_drawing_spec=drawing_spec,
+                    connection_drawing_spec=drawing_spec)
+        
+        cv2.imshow('MediaPipe FaceMesh', image)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:  # Press 'Esc' key to exit the loop
+            break
+
+cap.release()
+cv2.destroyAllWindows()
